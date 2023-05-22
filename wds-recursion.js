74233bf38f1bf208bf18a4a8657da844382f28b5
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-05-22 14:27:20 -0500

    Add countDownRecursive function

--- wds-recursion.js
+++ wds-recursion.js
@@ -1,1 +1,11 @@
 // Web Dev Simplified recursion exercises
+
+// count down from n and console log each step
+function countDownRecursive(n) {
+  if (n <= 0) {
+    console.log("Hooray");
+    return;
+  }
+  console.log(n);
+  countDownRecursive(n - 1);
+}

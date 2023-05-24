e8a97dc2723d3615abbcac041e6012fb54c9c94f
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-05-24 12:53:06 -0500

    Add productOfArray() function

--- codingame-recursion.js
+++ codingame-recursion.js
@@ -43,3 +43,21 @@ function factorial(n) {
   // recursive case
   return n * factorial(n - 1);
 }
+
+/* Question 4: Check all values in an array
+Write a function called all which accepts an array and a callback and returns true if 
+every value in the array returns true when passed as parameter to the callback function */
+
+/* Question 5: Product of an array
+Write a function called productOfArray which takes in an array of numbers and returns the 
+product of them all */
+
+function productOfArray(arr) {
+  // base case
+  if (arr.length === 0) {
+    return 1;
+  }
+
+  // recursive case
+  return arr.shift() * productOfArray(arr);
+}

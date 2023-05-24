92584fec4a15d08d009b8a6a3afa2e20145c1c0e
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-05-24 11:56:53 -0500

    Add factorial() function

--- codingame-recursion.js
+++ codingame-recursion.js
@@ -17,6 +17,7 @@ function sumRange(n) {
 /* Question 2: Power function
 Write a function called power which takes in a base and an exponent. If the exponent
 is 0, return 1. */
+
 function power(b, n) {
   // base case
   if (n === 0) {
@@ -25,4 +26,20 @@ function power(b, n) {
 
   // recursive case
   return b * power(b, n - 1);
+}
+
+/* Question 3: Calculate factorial
+Write a function that returns the factorial of a number. As a quick refresher, a factorial 
+of a number is the result of that number multiplied by the number before it, and 
+the number before that number, and so on, until you reach 1. 
+The factorial of 1 is just 1. */
+
+function factorial(n) {
+  // base case
+  if (n === 1) {
+    return 1;
+  }
+
+  // recursive case
+  return n * factorial(n - 1);
 }
